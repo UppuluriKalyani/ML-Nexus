d50e4231d87f7b832339c97082ee75c18b1a5547
Mehul <mehulprajapati2802@gmail.com>
2024-11-05 14:14:17 -0500

    Add about, contact pages with Routing

--- ml-nexus/src/utils/routing.jsx
+++ ml-nexus/src/utils/routing.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../components/Home'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Nav from '../components/Nav'
 
 function routing() {
   return (
-    <Routes>
-        <Route path='/' element={<Home />} />
-    </Routes>
-  )
+    <div>
+      {/* The Nav component is rendered on all pages */}
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default routing
